dbc744a05d8a501153d7bd57e68268483a7f34f5
Azizbek <jonibekov99@gmail.com>
2022-09-07 05:29:54 +0500

    Add average calories per user report

--- client/src/dashboard/dashboard-page.tsx
+++ client/src/dashboard/dashboard-page.tsx
@@ -1,5 +1,15 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  Brush,
+} from "recharts";
 import { getStatistics } from "../shared/api/admin";
 import { Stats } from "./stats";
 
@@ -14,6 +24,34 @@ export function DashboardPage() {
         </Box>
       </Flex>
       <Stats data={data} />
+      <Box height="400px" my="4">
+        <Heading mb="4" fontSize="md" fontWeight="semibold">
+          Average calories per user
+        </Heading>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data?.average_calories_per_user.data}
+          >
+            <XAxis dataKey="first_name" tick={{ fontSize: 12 }} />
+            <YAxis tickSize={10} tick={{ fontSize: 12 }} />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="average_calories"
+              name="Average calories"
+              fill="#82ca9d"
+            />
+            <Brush
+              dataKey="first_name"
+              height={30}
+              stroke="#8884d8"
+              endIndex={20}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </Box>
     </Box>
   );
 }
